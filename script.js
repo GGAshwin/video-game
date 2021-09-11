ed7eda5ad8975fb5705093b8173734e540f3cfda
G G ASHWIN PRABHU <65995972+playmaker159@users.noreply.github.com>
2021-09-11 14:03:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+/*function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}*/
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+/*var audioElement = new Audio('sleep.mp3');
+audioElement.play()
+*/
